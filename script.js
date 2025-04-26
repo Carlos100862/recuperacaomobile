6ca73ea63bb4b378a0c05866326be9c71a0bbf72
Carlos100862 <carlos.machado.cardozo@escola.pr.gov.br>
2025-04-26 20:13:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Lista de imagens e textos para o card
+let cardContent = [
+    { image: "woldwalking-removebg-preview.png", text: "Encontre milhares de possibilidades para sua próxima viagem" },
+    { image: "pigcoin-removebg-preview.png", text: "Receba bonificações por suas viagens e manuseie seu saldo no aplicativo" },
+    { image: "hotelbooking-removebg-preview.png", text: "Troque seus pontos acumulados por descontos ou prêmios." }
+];
+
+let currentIndex = 0;
+
+// Função para mudar a imagem e o texto do card
+function changeImage(direction) {
+    if (direction === 'left') {
+        currentIndex = (currentIndex === 0) ? cardContent.length - 1 : currentIndex - 1;
+    } else if (direction === 'right') {
+        currentIndex = (currentIndex === cardContent.length - 1) ? 0 : currentIndex + 1;
+    }
+
+    document.getElementById("card-image").src = cardContent[currentIndex].image;
+    document.getElementById("card-text").innerHTML = `<p>${cardContent[currentIndex].text}</p>`;
+}
